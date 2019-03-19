20ddaeed2809a481d8ab7893b9c92d856b782019
Dan Murphy <trinostics@users.noreply.github.com>
2019-03-19 16:00:08 -0700

    Update MultiChainLadder.R
    My second fis did not work. Reverts back to first fix, which did work.

--- R/MultiChainLadder.R
+++ R/MultiChainLadder.R
@@ -1268,9 +1268,14 @@ setMethod("fitted",
 	}
 	if (model=="MCL") {
 			B <- coef(object)
+#print(B)
+#print(p)
+#print(m)
     			for (i in 1:(n-1)){
         			x <- sapply(Triangles, "[", 1:(m-i),i)  
-        			x <- x[!is.na(x)]
+#print(i)
+#        			x <- x[!is.na(x), drop = FALSE]
+#print(x)
       			fitted[[i]] <- x%*%diag(B[[i]],nrow=p)
 			}
 	}
